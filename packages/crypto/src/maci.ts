48ebae02d05eedd139351b03715b58933d2592f7
99Kies <1290017556@qq.com>
2025-09-07 00:40:46 +0800

    feat(crypto): add build vote message func

--- packages/crypto/src/maci.ts
+++ packages/crypto/src/maci.ts
@@ -1,10 +1,23 @@
+import CryptoJS from 'crypto-js';
 import { MaciAccount } from './account';
-import { batchGenMessage, packPubKey, unpackPubKey } from './crypto';
+import {
+	batchGenMessage,
+	packPubKey,
+	unpackPubKey,
+	bigInt2Buffer,
+	genEcdhSharedKey,
+	genKeypair,
+} from './crypto';
+import { poseidon } from './crypto/hashing';
+import { poseidonEncrypt } from '@zk-kit/poseidon-cipher';
+import { signMessage } from '@zk-kit/eddsa-poseidon';
 import {
 	ClientParams,
 	CertificateEcosystem,
 	DerivePathParams,
 	PubKey,
+	PrivKey,
+	Keypair,
 } from './types';
 
 /**
@@ -89,12 +102,18 @@ export class MaciClient {
 	}
 
 	async buildVotePayload({
+		stateIdx,
+		operatorPubkey,
 		selectedOptions,
+		derivePathParams,
 	}: {
+		stateIdx: number;
+		operatorPubkey: bigint;
 		selectedOptions: {
 			idx: number;
 			vc: number;
 		}[];
+		derivePathParams?: DerivePathParams;
 	}) {
 		const options = await this.processVoteOptions({
 			selectedOptions,
@@ -105,12 +124,106 @@ export class MaciClient {
 			return [o.idx, o.vc] as [number, number];
 		});
 
-		// const payload = batchGenMessage(
-		// 	stateIdx,
-		// 	this.getSigner(),
-		// 	operatorCoordPubKey,
-		// 	plan
-		// );
+		const payload = this.batchGenMessage(
+			stateIdx,
+			operatorPubkey,
+			plan,
+			derivePathParams
+		);
+
+		return payload;
+	}
+
+	async batchGenMessage(
+		stateIdx: number,
+		operatorPubkey: bigint,
+		plan: [number, number][],
+		derivePathParams?: DerivePathParams
+	) {
+		const genMessage = this.genMessageFactory(
+			stateIdx,
+			operatorPubkey,
+			derivePathParams
+		);
+
+		const payload = [];
+		for (let i = plan.length - 1; i >= 0; i--) {
+			const p = plan[i];
+			const encAccount = genKeypair();
+			const msg = genMessage(
+				BigInt(encAccount.privKey),
+				i + 1,
+				p[0],
+				p[1],
+				i === plan.length - 1
+			);
+
+			payload.push({
+				msg,
+				encPubkeys: encAccount.pubKey,
+			});
+		}
+
+		return payload;
+	}
+
+	genMessageFactory(
+		stateIdx: number,
+		operatorPubkey: bigint,
+		// signPriKey: PrivKey,
+		// signPubKey: PubKey,
+		// coordPubKey: PubKey,
+		derivePathParams?: DerivePathParams
+	) {
+		return (
+			encPriKey: PrivKey,
+			nonce: number,
+			voIdx: number,
+			newVotes: number,
+			isLastCmd: boolean,
+			salt?: bigint
+		): bigint[] => {
+			if (!salt) {
+				// uint56
+				salt = BigInt(
+					`0x${CryptoJS.lib.WordArray.random(7).toString(CryptoJS.enc.Hex)}`
+				);
+			}
+
+			const packaged =
+				BigInt(nonce) +
+				(BigInt(stateIdx) << 32n) +
+				(BigInt(voIdx) << 64n) +
+				(BigInt(newVotes) << 96n) +
+				(BigInt(salt) << 192n);
+
+			const signer = this.getSigner(derivePathParams);
+
+			let newPubKey = [...signer.getPublicKey().toPoints()];
+			if (isLastCmd) {
+				newPubKey = [0n, 0n];
+			}
+
+			const hash = poseidon([packaged, ...newPubKey]);
+			// const signature = signMessage(bigInt2Buffer(signPriKey), hash);
+			const signature = signer.sign(hash);
+
+			const command = [
+				packaged,
+				...newPubKey,
+				...signature.R8,
+				signature.S,
+			];
+			const coordPubkey = this.unpackMaciPubkey(operatorPubkey);
+
+			const message = poseidonEncrypt(
+				command,
+				genEcdhSharedKey(encPriKey, coordPubkey),
+				0n
+			);
+
+			return message;
+		};
 	}
 
 	// async buildAddNewKeyPayload() {}
