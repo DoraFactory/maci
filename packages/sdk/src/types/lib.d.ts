6b4eac47c317251bd71c1bb4c5c5bd2751b6c466
99Kies <1290017556@qq.com>
2025-10-13 15:16:23 +0800

    feat(sdk): update adapter with noble/curves

--- packages/sdk/src/types/lib.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'ffjavascript' {
-	export const utils: any;
-	// eslint-disable-next-line @typescript-eslint/naming-convention
-	export const Scalar: any;
-	export const buildBn128: any;
-	export const buildBls12381: any;
-}
