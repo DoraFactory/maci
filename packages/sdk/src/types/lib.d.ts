e89195b285289297edccd898bb774fafd29edced
99Kies <1290017556@qq.com>
2025-10-13 16:05:58 +0800

    feat(sdk): reuse ffjavascript

---
+++ packages/sdk/src/types/lib.d.ts
@@ -0,0 +1,7 @@
+declare module 'ffjavascript' {
+  export const utils: any;
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  export const Scalar: any;
+  export const buildBn128: any;
+  export const buildBls12381: any;
+}
